9f2e98fb6bda2dee6fa4a143f98d40bc28743c3b
GitHub Action <action@github.com>
2021-10-07 12:45:38 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kawamataryo/intellijpluginsandbox/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kawamataryo.intellijpluginsandbox.services
+
+import com.github.kawamataryo.intellijpluginsandbox.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
